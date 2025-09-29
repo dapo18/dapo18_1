f0cfb3eeb08abd4420b9af8051e2e0d34727d610
dapo18 <terehinmisa299@gmail.com>
2025-09-29 23:12:53 +0300

    2.py

--- 2.py
+++ 2.py
@@ -9,30 +9,43 @@ print(heapq.heappop(heap))
 
 
 #Биномиальная куча
-class BinomialNode:
-    def __init__(self, key):
-        self.key = key
-        self.degree = 0
-        self.parent = None
-        self.child = None
-        self.sibling = None
-
 class BinomialHeap:
     def __init__(self):
-        self.head = None
+        self.trees = []
+        self.min_node = None
     
-    def merge(self, h2):
-        # Объединение двух куч
+    def insert(self, value):
         pass
     
-    def insert(self, key):
-        new_heap = BinomialHeap()
-        new_heap.head = BinomialNode(key)
-        self.merge(new_heap)
+    def merge(self, other_heap):
+        pass
     
     def extract_min(self):
-        # Извлечение минимального элемента
         pass
 
+#Куча фибоначчи
+# Требуется установка: pip install fibheap
+import fibheap
 
-#
+fh = fibheap.makefheap()
+fibheap.fheappush(fh, 5)
+fibheap.fheappush(fh, 2)
+fibheap.fheappush(fh, 8)
+min_val = fibheap.fheappop(fh)  # 2
+
+
+#Хэш-таблицы
+hash_table = {}
+hash_table["key1"] = "value1"
+hash_table["key2"] = "value2"
+
+value = hash_table.get("key1")
+
+if "key1" in hash_table:
+    print("Key exists")
+
+del hash_table["key1"]
+
+from collections import defaultdict
+dd = defaultdict(int)
+dd["count"] += 1
